54c0bbc95090730a70a8225a1e15db994338de5b
xander81992 <xander081992@gmail.com>
2026-06-23 22:21:30 -0400

    Upgrade Firebase SDK and update configuration

--- auth.js
+++ auth.js
@@ -1,17 +1,22 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBhCpX9q1iHWKZ5AbfrJWRpaCWNvKT3nJM",
+  authDomain: "yard-system.firebaseapp.com",
+  projectId: "yard-system",
+  storageBucket: "yard-system.firebasestorage.app",
+  messagingSenderId: "42005950110",
+  appId: "1:42005950110:web:4de843b6356cb0b3d2958f",
+  measurementId: "G-0FKFZL3XE2"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const analytics = getAnalytics(app);
 
-// IMPORTANT: export it properly
+// login function
 export async function loginUser(email, password) {
   return await signInWithEmailAndPassword(auth, email, password);
 }
